37872e142306fa561e9fc7d4c47fb8e1cfe93f0c
Pedram <pedram.esmaeeli@gmail.com>
2025-03-19 18:45:27 +0100

    Remove code from After

--- src/main/java/com/github/pdrum/java101/p05/functions/example/After.java
+++ src/main/java/com/github/pdrum/java101/p05/functions/example/After.java
@@ -11,46 +11,21 @@ public class After {
 
         System.out.println("Enter threshold (exclusive)");
         int threshold = scanner.nextInt();
-
-        if ("even".equals(nameOfFunctionality)) {
-            printSumOfEvenNumbersUntil(threshold);
-        } else if ("prime".equals(nameOfFunctionality)) {
-            printSumOfPrimeNumbersUntil(threshold);
-        } else {
-            System.out.println("Unsupported");
-        }
     }
 
     public static void printSumOfEvenNumbersUntil(int exclusiveThreshold) {
-        int sum = 0;
-        for (int number = 0; number < exclusiveThreshold; number++) {
-            if (isEven(number)) {
-                sum += number;
-            }
-        }
-        System.out.println(sum);
+
     }
 
     public static boolean isEven(int number) {
-        return number % 2 == 0;
+        return false;
     }
 
     public static void printSumOfPrimeNumbersUntil(int exclusiveThreshold) {
-        int sum = 0;
-        for (int number = 0; number < exclusiveThreshold; number++) {
-            if (isPrime(number)) {
-                sum += number;
-            }
-        }
-        System.out.println(sum);
+
     }
 
     public static boolean isPrime(int number) {
-        for (int smallerNumber = 2; smallerNumber < number; smallerNumber++) {
-            if (number % smallerNumber == 0) {
-                return false;
-            }
-        }
-        return number != 0 && number != 1;
+        return false;
     }
 }
