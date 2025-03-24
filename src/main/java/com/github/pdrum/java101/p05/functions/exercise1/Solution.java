9276b6c57a0ae6b7fa5b50919c70451df0a65058
paniz <zzpanyzz@gmail.com>
2025-03-24 22:15:18 +0330

    add solution

---
+++ src/main/java/com/github/pdrum/java101/p05/functions/exercise1/Solution.java
@@ -0,0 +1,60 @@
+package com.github.pdrum.java101.p05.functions.exercise1;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What type of number do you want to add up:");
+        String nameOfFunctionality = scanner.nextLine();
+
+        System.out.println("Enter threshold (exclusive)");
+        int threshold = scanner.nextInt();
+
+        if ("even".equals(nameOfFunctionality)) {
+            printSumOfEvenNumbersUntil(threshold);
+
+        } else if ("prime".equals(nameOfFunctionality)) {
+            printSumOfPrimeNumbersUntil(threshold);
+        } else {
+            System.out.println("Unsupported");
+        }
+    }
+
+    public static void printSumOfEvenNumbersUntil(int exclusiveThreshold) {
+        int sum = 0;
+        for (int number = 0; number < exclusiveThreshold; number++) {
+            if (isEven(number)) {
+                sum += number;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static void printSumOfPrimeNumbersUntil(int exclusiveThreshold) {
+        int sum = 0;
+        for (int number = 0; number < exclusiveThreshold; number++) {
+         if (isPrime(number)) {
+                sum += number;
+         }
+        }
+        System.out.println(sum);
+    }
+
+    public static boolean isPrime(int number) {
+        if (number == 0 || number == 1) {
+            return false;
+        }
+        for (int smallerNumber = 2; smallerNumber < number; smallerNumber++) {
+            if (number % smallerNumber == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
