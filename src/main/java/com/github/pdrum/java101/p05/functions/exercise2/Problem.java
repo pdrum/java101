7a11da782d0672606ec25fa1f23421badc8c7b7d
Pedram <pedram.esmaeeli@gmail.com>
2025-03-24 20:48:15 +0100

    Add the problem

---
+++ src/main/java/com/github/pdrum/java101/p05/functions/exercise2/Problem.java
@@ -0,0 +1,22 @@
+package com.github.pdrum.java101.p05.functions.exercise2;
+
+import java.util.Scanner;
+
+public class Problem {
+
+    public static void main(String[] args) {
+        System.out.println("Enter three numbers");
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+        System.out.println("The sum of the three numbers is:");
+        // Problem 1) Write a function that gets
+        // three variables and returns
+        // their sum. Print the returned value here
+
+        // Problem 2) Write a function that returns the maximum
+        // of the three numbers. Print the output here.
+
+    }
+}
